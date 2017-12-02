b1beafff575a33c2f812037a5be57e000c57020f
moxinol <moxikuma@gmail.com>
2017-12-02 13:53:10 +0000

    created KeyInput class

---
+++ AShootingGame/src/org/moxinol/ashootinggame/KeyInput.java
@@ -0,0 +1,17 @@
+package org.moxinol.ashootinggame;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+/**
+ * Created by moxinol on 12/2/2017.
+ */
+public class KeyInput extends KeyAdapter{
+
+    public void keyPressed(KeyEvent e){
+
+    }
+    public void keyReleasead(KeyEvent e){
+
+    }
+}
